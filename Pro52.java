bfb3464a3ba5efb9c652369f82e4dab26152a0e8
guvian <gayathri.devicse@ksrce.ac.in>
2016-08-29 11:55:23 +0530

    Update Pro52.java

--- Pro52.java
+++ Pro52.java
@@ -4,17 +4,28 @@ public class Pro52 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int[] a = new int[8];
-		System.out.println("Enter the four points(8 values) :");
+		System.out.println("Enter the four Co_ordinates(8 values) :");
 		for (int i = 0; i < a.length; i++) {
 			a[i] = scanner.nextInt();
 		}
-		double r = Math.sqrt(((a[6] - a[4]) * (a[6] - a[4])) + ((a[7] - a[5]) * (a[7] - a[5])));
-		double p = Math.sqrt(((a[2] - a[0]) * (a[2] - a[0])) + ((a[3] - a[1]) * (a[3] - a[1])));
-		double q = Math.sqrt(((a[4] - a[2]) * (a[4] - a[2])) + ((a[5] - a[3]) * (a[5] - a[3])));
-		if (p == q && q == r) {
-			System.out.println("The given four points forms a square.");
+		for (int j = 2; j < a.length; j = j + 2) {
+			if (a[0] / a[j] == 1) {
+				int temp = a[2];
+				int temp1 = a[3];
+				a[2] = a[j];
+				a[3] = a[j + 1];
+				a[j] = temp;
+				a[j + 1] = temp1;
+			}
+		}
+int p = Math.abs(a[1] - a[3]);
+		int q = Math.abs(a[5] - a[7]);
+		int r = Math.abs(a[2] - a[4]);
+		int s = Math.abs(a[0] - a[6]);
+	if (p == q && p == r && p == s) {
+			System.out.println("The given Co_ordinates forms a square.");
 		} else {
-			System.out.println("The given four points does not forms a square.");
+			System.out.println("The given Co_ordinates does not forms a square.");
 		}
 		scanner.close();
 	}
